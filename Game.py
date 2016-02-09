12c2ac4621e3b2c550a7536d772f30f584562bee
ncarlson <nc20213@gmail.com>
2016-02-09 11:24:27 -0800

    Student table built at /students

--- Game.py
+++ Game.py
@@ -4,6 +4,8 @@ Created on Feb 9, 2016
 @author: ncarlson
 '''
 
+import Courses
+
 class Game(object):
     '''
     classdocs
@@ -14,4 +16,10 @@ class Game(object):
         '''
         Constructor
         '''
-        self.students = ["Test"]
+        self.students = []
+        ################Student Test##################
+        student = Courses.Student("Nick","Carlson",1)
+        student2 = Courses.Student("Vila","Kongpachith",2)
+        self.students.append(student) 
+        self.students.append(student2)       
+        ##############################################
